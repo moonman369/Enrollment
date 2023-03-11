85311df14293262a5f958237d9b84a433498f390
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-03-11 18:30:20 +0530

    Add db model object to scripts - Add page to display user creds

--- config.py
+++ config.py
@@ -2,3 +2,10 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRECT_KEY') or "yomommasofatshebendslight"
+
+    MONGODB_SETTINGS = {
+        'db' : 'UTA_Enrollment',
+        # 'host': 'mongodb://localhost:27017/UTA-Enrollment'
+    }
+
+
