e3e2eb76b7786860b92982f571af8fa331868dec
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-03-13 14:32:57 +0530

    Add more validation, length, equality and type checks to forms

--- application/forms.py
+++ application/forms.py
@@ -1,19 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from application.models import User
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired(), Length(min=8, max=15)])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField("Login")
 
+
 class RegisterForm(FlaskForm):
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name', validators=[DataRequired()])
-    email= StringField('Email', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
-    password_confirm = StringField('Confirm Password', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=1, max=55)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=1, max=55)])
+    email= StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired(), Length(min=8, max=15)])
+    password_confirm = StringField('Confirm Password', validators=[DataRequired(), Length(min=8, max=15), EqualTo('password')])
     submit = SubmitField('Register Now')
+
+    def validate_email(self, email):
+        user = User.objects(email=email.data).first()
+        if user:
+            raise ValidationError('Email is already in use. Choose an different email and try again.')
 
     
